79b1f5bc73f0b2db5eeffef286662fc247302059
awsphani <awsphani@gmail.com>
2022-03-14 12:01:54 -0700

    Update sparkdatabrkscert.py

--- sparkdatabrkscert.py
+++ sparkdatabrkscert.py
@@ -192,3 +192,91 @@ dummy(user_details)
 ([1, 'Scott'],)
 1
 
+
+#08 Overview of Basic Data Types in Spark
+
+import datetime
+users = [
+    {
+        "id": 1,
+        "first_name": "Corrie",
+        "last_name": "Van den Oord",
+        "email": "cvandenoord0@etsy.com",
+        "is_customer": True,
+        "amount_paid": 1000.55,
+        "customer_from": datetime.date(2021, 1, 15),
+        "last_updated_ts": datetime.datetime(2021, 2, 10, 1, 15, 0)
+    },
+    {
+        "id": 2,
+        "first_name": "Nikolaus",
+        "last_name": "Brewitt",
+        "email": "nbrewitt1@dailymail.co.uk",
+        "is_customer": True,
+        "amount_paid": 900.0,
+        "customer_from": datetime.date(2021, 2, 14),
+        "last_updated_ts": datetime.datetime(2021, 2, 18, 3, 33, 0)
+    },
+    {
+        "id": 3,
+        "first_name": "Orelie",
+        "last_name": "Penney",
+        "email": "openney2@vistaprint.com",
+        "is_customer": True,
+        "amount_paid": 850.55,
+        "customer_from": datetime.date(2021, 1, 21),
+        "last_updated_ts": datetime.datetime(2021, 3, 15, 15, 16, 55)
+    },
+    {
+        "id": 4,
+        "first_name": "Ashby",
+        "last_name": "Maddocks",
+        "email": "amaddocks3@home.pl",
+        "is_customer": False,
+        "amount_paid": None,
+        "customer_from": None,
+        "last_updated_ts": datetime.datetime(2021, 4, 10, 17, 45, 30)
+    },
+    {
+        "id": 5,
+        "first_name": "Kurt",
+        "last_name": "Rome",
+        "email": "krome4@shutterfly.com",
+        "is_customer": False,
+        "amount_paid": None,
+        "customer_from": None,
+        "last_updated_ts": datetime.datetime(2021, 4, 2, 0, 55, 18)
+    }
+]
+from pyspark.sql import Row
+
+users_df = spark.createDataFrame([Row(**user) for user in users])
+users_df.printSchema()
+'''
+root
+ |-- id: long (nullable = true)
+ |-- first_name: string (nullable = true)
+ |-- last_name: string (nullable = true)
+ |-- email: string (nullable = true)
+ |-- is_customer: boolean (nullable = true)
+ |-- amount_paid: double (nullable = true)
+ |-- customer_from: date (nullable = true)
+ |-- last_updated_ts: timestamp (nullable = true)
+'''
+users_df.columns
+'''
+['id','first_name','last_name','email','is_customer','amount_paid','customer_from','last_updated_ts']
+'''
+users_df.dtypes
+'''
+[('id', 'bigint'),
+ ('first_name', 'string'),
+ ('last_name', 'string'),
+ ('email', 'string'),
+ ('is_customer', 'boolean'),
+ ('amount_paid', 'double'),
+ ('customer_from', 'date'),
+ ('last_updated_ts', 'timestamp')]
+'''
+
+
